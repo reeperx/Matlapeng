e5f984878e95f7d4cf99204d8dc8c5c2a9bdc5ba
reeperx10 <acebany080@gmail.com>
2024-04-05 12:55:54 +0200

    first commit

---
+++ app/(dashboard)/(routes)/live/page.tsx
@@ -0,0 +1,9 @@
+const LivePage = () => {
+    return ( 
+        <div>
+            Live PAge
+        </div>
+     );
+}
+ 
+export default LivePage;
---
+++ app/(dashboard)/(routes)/teacher/live/page.tsx
@@ -0,0 +1,9 @@
+const LivePage = () => {
+    return ( 
+        <div>
+            Live Page
+        </div>
+     );
+}
+ 
+export default LivePage;
